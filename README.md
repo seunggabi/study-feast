a270d4cd16b14203e3b491fe35067954428d9b34
seunggabi <seunggabi@naver.com>
2023-01-30 19:54:26 +0900

    feat: offline

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # study-feast
+
+```shell
+pip3 install -r requirements.txt
+
+feast version
+
+feast init asdf
+cd asdf
+feast apply
+ll data
+```
---
+++ asdf/offline.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from datetime import datetime
+
+entity_df = pd.DataFrame.from_dict(
+    {
+        "driver_id": [1001, 1002, 1003, 1004],
+        "event_timestamp": [
+            datetime(2021, 4, 12, 10, 59, 42),
+            datetime(2021, 4, 12, 8, 12, 10),
+            datetime(2021, 4, 12, 16, 40, 26),
+            datetime(2021, 4, 12, 15, 1, 12),
+        ],
+    }
+)
+
+print(entity_df.head())
+
+from feast import FeatureStore
+
+store = FeatureStore(repo_path=".")
+
+training_df = store.get_historical_features(
+    entity_df=entity_df,
+    features=[
+        "driver_hourly_stats:conv_rate",
+        "driver_hourly_stats:acc_rate",
+        "driver_hourly_stats:avg_daily_trips",
+    ],
+).to_df()
+
+print(training_df.head())
